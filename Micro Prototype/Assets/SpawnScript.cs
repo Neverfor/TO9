307d57d73d1b5d6964f8e48a90a301472d225c10
Neverfor <icelance@hotmail.ru>
2014-04-11 13:20:00 +0200

    First comm
    comm

---
+++ Micro Prototype/Assets/SpawnScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+
+    public int amountOfBacteries = 10;
+    public Transform SpawnSpot;
+    //public Time TimeOut = 10.0F;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
